7fdb38fd2facadb26d0d7b136756a3cb4c055612
hmoss333 <hdamoss@gmail.com>
2018-03-09 14:51:53 -0500

    Update 3/9/2018 pt.3
    -Loading screen fixes

--- Assets/Scripts/MiniGame/StoryCombos.cs
+++ Assets/Scripts/MiniGame/StoryCombos.cs
@@ -314,7 +314,7 @@ public class StoryCombos : MonoBehaviour {
         spinCount = 0;
         jumpCount = 0;
         shrinkCount = 0;
-        help.text = "Dog and Cat can <b><color=yellow>Jump</color></b>, and <b><color=yellow>Spin</color></b>! Enter the contest, can you win?";
+        help.text = "Lion and Elephant can <b><color=yellow>Jump</color></b>, and <b><color=yellow>Spin</color></b>! Enter the contest, can you win?";
         playSound(2);
         movementLengthCollection = 0;
         movement.Clear();
